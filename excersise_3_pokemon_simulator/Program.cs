78886bda0508978b519f8b72bccccfca569d5151
with-user-a-name <micke_999999999@protonmail.com>
2025-11-11 17:23:26 +0100

    Add project files.

---
+++ excersise_3_pokemon_simulator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace excersise_3_pokemon_simulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
