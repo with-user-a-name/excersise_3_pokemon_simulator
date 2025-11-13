5d40b6bfe515c5cd9544cb042e682e0fee2e80a2
with-user-a-name <micke_999999999@protonmail.com>
2025-11-13 18:49:27 +0100

    WIP: Pokemon class - mainly props & ctor
    * Implemented validation check for Pokemon Name,Level and set the Type in the FirePokemon subclass.
      Some temporary code lines for testing the Pokemon props remain in Main.Init().
    * Extracted class Pokemon from Main.cs to Pokemon.cs.
    * Added 4 additional attacks for a total of 6: flamethrower, ember, poison, cut, choke, tsunami

--- excersise_3_pokemon_simulator/Main.cs
+++ excersise_3_pokemon_simulator/Main.cs
@@ -16,44 +16,11 @@ namespace excersise_3_pokemon_simulator
     }
 
 
-    abstract class Pokemon
-    {
-        private List<Attack>? _attacks;
-
-        //TODO 2511121551: Validate that it is 2–15 characters.
-        public int Name { get; set; }
-        //TODO 2511121552: Validate that it is greater than 1 (>=1).
-        public uint Level{ get; set; }
-        public ElementType Type { get; set; }
-
-        protected Pokemon(List<Attack> attacks)
-        {
-            //TODO 2511121559: Make a deep copy of attacks in _attacks ?!
-            //Or whats the purpose, how will it be used...
-        }
-
-        public void RandomAttack()
-        {
-            // Selects a random attack from the list and calls its .Use method.
-        }
-
-        public void Attack()
-        {
-            // Allows the user to choose an attack from the list and calls its .Use method.
-        }
-
-        public void RaiseLevel()
-        {
-            // Increases the Pokémon's level and prints that it has leveled up.
-        }
-    }
-
-
     class FirePokemon : Pokemon
     {
         public FirePokemon(List<Attack> attacks) : base(attacks)
         {
-
+            Type = ElementType.Fire;
         }
     }
 
@@ -86,15 +53,69 @@ namespace excersise_3_pokemon_simulator
         public void Init()
         {
             // 3. Create at least two attacks per type.
-            Attack flamethrower = new Attack(name: "Flamethrower", ElementType.Fire, basePower: 12, _ui);
-            Attack ember        = new Attack(name: "Ember",        ElementType.Fire, basePower:  6, _ui);
+            Attack flamethrower = new Attack(name: "Flamethrower", ElementType.Fire,  basePower: 12, _ui);
+            Attack ember        = new Attack(name: "Ember",        ElementType.Fire,  basePower:  6, _ui);
+            Attack poison       = new Attack(name: "Poison",       ElementType.Grass, basePower: 18, _ui);
+            Attack cut          = new Attack(name: "Cut",          ElementType.Grass, basePower: 11, _ui);
+            Attack choke        = new Attack(name: "Choke",        ElementType.Water, basePower:  9, _ui);
+            Attack tsunami      = new Attack(name: "Tsunami",      ElementType.Water, basePower: 24, _ui);
 
             // Just a quick test.
             flamethrower.Use(10);
             ember.Use(20);
 
-            _pokemons = new List<Pokemon>();
+            //TODO 2511131841: The pkMnAttacks list has to be secured.
+            var pkMnAttacks = new List<Attack>();
+            pkMnAttacks.Add(flamethrower);
+            pkMnAttacks.Add(ember);
+            // Try make a Pokemon and see how the consrtuction behaves.
+            //var pkMn = new FirePokemon(pkMnAttacks);
+            //pkMnAttacks = null;
+            var pkMn = new FirePokemon(pkMnAttacks);
+
+            string? pkMnName;
+            pkMnName = "123456789012345";
+
+            _ui.WrLn($"nameof(pkMnName):        \"{nameof(pkMnName)}\"");
+            _ui.WrLn($"nameof(pkMnName.Length): \"{nameof(pkMnName.Length)}\"");
+
+            //pkMnName = "123456789012345";
+            _ui.WrLn($"Setting pokemoin name to \"{pkMnName}\" that is {pkMnName.Length} long.");
+            pkMn.Name = pkMnName;
+            pkMnName = "12";
+            _ui.WrLn($"Setting pokemoin name to \"{pkMnName}\" that is {pkMnName.Length} long.");
+            pkMn.Name = pkMnName;
+            //pkMnName = "1234567890123456";
+            //_ui.WrLn($"Setting pokemoin name to \"{pkMnName}\" that is {pkMnName.Length} long.");
+            //pkMn.Name = pkMnName;
+            //pkMnName = "1";
+            //_ui.WrLn($"Setting pokemoin name to \"{pkMnName}\" that is {pkMnName.Length} long.");
+            //pkMn.Name = pkMnName;
+
+            //pkMnName = null;
+            //_ui.WrLn($"Setting pokemon name to null.");
+            //pkMn.Name = pkMnName;
+
+            //pkMnName = "";
+            //_ui.WrLn($"Setting pokemoin name to \"{pkMnName}\" that is {pkMnName.Length} long.");
+            //pkMn.Name = pkMnName;
+
+            pkMn.Name = "Carl Af Anka";
+            _ui.WrLn($"The pokemon name is: \"{pkMn.Name}\"");
+
+            pkMn.Level = uint.MaxValue;
+            _ui.WrLn($"The pokemon level is: \"{pkMn.Level}\"");
+
+            _ui.WrLn($"The pokemon type is: \"{pkMn.Type}\"");
+
+
+
             //TODO 2511121648: Create some pokemons and put them in the list
+            _pokemons = new List<Pokemon>();
+            _pokemons.Add(pkMn);
+
+
+
         }
 
         public void Run()
---
+++ excersise_3_pokemon_simulator/Pokemon.cs
@@ -0,0 +1,71 @@
+﻿namespace excersise_3_pokemon_simulator
+{
+    abstract class Pokemon
+    {
+        private List<Attack>? _attacks;
+
+        //TODO 2511131808: Perhaps extract min/max values for a prop to an enum?
+        private const int _levelMinVal = 1;
+        private const int _nameMaxLen = 15;
+        private const int _nameMinLen = 2;
+
+        private uint _level;
+        private string _name = "";
+
+        public uint Level
+        {
+            get => _level;
+            set
+            {
+                ArgumentOutOfRangeException.ThrowIfLessThan<uint>
+                    (value, _levelMinVal, nameof(value));
+                _level = value;
+            }
+        }
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                //The Name property must contain between 2 < 15 characters.
+                ArgumentException.ThrowIfNullOrWhiteSpace(value);
+                ArgumentOutOfRangeException.ThrowIfGreaterThan<int>
+                    (value.Length
+                    ,_nameMaxLen
+                    ,$"{nameof(value)}.{nameof(value.Length)}");
+                ArgumentOutOfRangeException.ThrowIfLessThan<int>
+                    (value.Length
+                    ,_nameMinLen
+                    ,$"{nameof(value)}.{nameof(value.Length)}");
+                _name = value;
+            }
+        }
+
+        public ElementType Type { get; protected set; }
+
+        protected Pokemon(List<Attack> attacks)
+        {
+            //TODO 2511121559: Make a deep copy of attacks in _attacks ?!
+            //Or whats the purpose, how will it be used... Nope, the attacks list shall be locked down as much as possible.
+            ArgumentNullException.ThrowIfNull(attacks);
+            //TODO 2511131836: Should a check be made to ensure the type of the attacks match the type of the pokemon?
+            this._attacks = attacks;
+        }
+
+        public void RandomAttack()
+        {
+            // Selects a random attack from the list and calls its .Use method.
+        }
+
+        public void Attack()
+        {
+            // Allows the user to choose an attack from the list and calls its .Use method.
+        }
+
+        public void RaiseLevel()
+        {
+            // Increases the Pokémon's level and prints that it has leveled up.
+        }
+    }
+}
